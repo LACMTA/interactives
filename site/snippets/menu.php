2962528eb2c850348c0c249767bb003853a3a6ea
Douglas Goodwin <dgoodwin@gmail.com>
2015-06-26 18:24:54 -0700

    added menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -3,25 +3,21 @@
 
   <li><a href="#hero" class="hidden">Top</a></li>
 
-  <li><a href="/">Metro Interactives: <?php echo $site->title() ?></a></li>
+  <li><a href="/">Metro Interactives: <?php echo $site->title() ?>&nbsp;|</a></li>
 
-  <!-- <?php foreach($pages->visible() as $p): ?>
-    <li><a href="<?php echo $p->url() ?>"><?php echo $p->uid() ?></a></li>
+  <?php 
+    $items = $pages->visible()->not('hero');
+    foreach($items as $p):
+  ?>
+    <li><a href="#<?php echo $p->uid() ?>"><?php echo $p->uid() ?></a></li>
   <?php endforeach ?>
- -->
+ 
 
-
-  <!-- DROPDOWN MENU -->
-  <!-- <li class="dropdown dropdown-hover">
-    <a href="#" class="dropdown-toggle external" data-toggle="dropdown" role="button" aria-expanded="false">Quick <span class="caret"></span></a>
-    <ul class="dropdown-menu" role="menu">
-      <?php foreach($pages->visible() as $p): ?>
-        <li><a href="<?php echo $p->url() ?>" class="anchor-link external"><?php echo $p->uid() ?></a></li>
-      <?php endforeach ?>
-    </ul>
-  </li> -->
 
   <!-- HEADER ACTION BUTTON -->
   <!-- <li class="header-action-button"><a href="#" class="btn btn-primary">Action Button</a></li> -->
 
 </ul>
+
+
+
